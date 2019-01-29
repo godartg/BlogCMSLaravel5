72ee308071a63ae83333edd7ff910104532f1123
workholic <sl.vilcapuma.edwin@gmail.com>
2019-01-29 12:33:17 -0500

    Cap 6 - paginate, auth

---
+++ resources/views/web/posts.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="col-md-8 col-md-offset-2">
+        <h1>Lista de artículos</h1>
+        @foreach($posts as $post)
+            <div class="panel panel-default">
+                <div class=panel-heading>
+                {{ $post->name}}
+                </div>
+            </div>
+            <div class="panel body">
+                @if($post->file)
+                <img src="{{ $post->file }}" class="img-responsive" alt="imagen">
+                @endif
+                {{ $post->excerpt }}
+                <a href="#" class="pull-rigth">Leer más</a>
+            </div>
+        @endforeach
+        {{ $posts->render() }}
+    </div>
+</div>
+@endsection
