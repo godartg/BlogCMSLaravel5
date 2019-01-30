b9b71603b78c2fa300b81430ddcf02e2ae7c7ea7
workholic <sl.vilcapuma.edwin@gmail.com>
2019-01-30 09:27:38 -0500

    Cap 8. ORM Eloquent

--- resources/views/web/post.blade.php
+++ resources/views/web/post.blade.php
@@ -8,24 +8,24 @@
             <div class="panel panel-default">
                 <div class=panel-heading>
                     Categoría
-                    <a href="#">{{ $post->category->name }}</a>
+                    <a href="{{ route('category', $post->category->slug) }}">{{$post->category->name}}</a>
                 </div>
-                </div>
-                <div class="panel body">
-                    @if($post->file)
-                    <img src="{{ $post->file }}" class="img-responsive" alt="imagen">
-                    @endif
-                    {{ $post->excerpt }}
-                    <hr>
+            </div>
+            <div class="panel body">
+                @if($post->file)
+                    <img src="{{ $post->file }}" class="img-responsive" >
+                @endif
+                {{ $post->excerpt }}
+                <hr>
                     {!! $post->body !!}
-                    <hr>
+                <hr>
                     Etiquetas
-                    @foreach($post->tags as $tag)
-                    <a href="#">
-                        {{ $tag->name }}
+                @foreach($post->tags as $tag)
+                    <a href="{{ route('tag', $tag->slug) }}">
+                    {{ $tag->name }}
                     </a>
-                    @endforeach
-                </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@
 |
 */
 
-Route::redirect('/', 'blog');
+Route::get('/', function () {
+    return redirect()->route('blog');
+});
 
 Auth::routes();
 
+//web
 Route::get('blog', 'Web\PageController@blog')->name('blog');
+Route::get('entrada/{slug}', 'Web\PageController@post')->name('post');
+Route::get('categoria/{slug}', 'Web\PageController@category')->name('category');
+Route::get('etiqueta/{slug}', 'Web\PageController@tag')->name('tag');
 
-Route::get('blog/{slug}', 'Web\PageController@post')->name('post');
+//admin
